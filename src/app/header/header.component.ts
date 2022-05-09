8a42d1c8ea2f818e6b48f5b0e4ec37ad807751b2
Andre Rocco Drisostes <andre.rocco@profusion.mobi>
2022-05-09 18:21:36 -0300

    feat: add header text and style

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  // TODO: implement component and remove eslint disable comments
+export class HeaderComponent {
+  batteryLife = 0;
+  firmwareVersion = '0.0.00';
+  title = 'Model11';
+
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method, @typescript-eslint/no-empty-function
-  ngOnInit(): void {}
 }

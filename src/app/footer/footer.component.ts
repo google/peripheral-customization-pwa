7d56dfe917c4baba682dc7eeb43f2fa9b4e673f4
MatheusCod <matheus.fernandes@profusion.mobi>
2022-05-09 16:37:10 -0300

    feat: add basic footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
-  // TODO: implement component and remove eslint disable comments
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method, @typescript-eslint/no-empty-function
-  ngOnInit(): void {}
+export class FooterComponent {
+  @Input() imgSource = 'assets/images/vendor1/footer-logo.svg';
 }

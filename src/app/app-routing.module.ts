b0e21893c1df0158584c4c7ca68ec5bb1abe4560
Andre Rocco Drisostes <andre.rocco@profusion.mobi>
2022-05-11 11:51:33 -0300

    feat: add routing to tabs path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DummyComponent } from './dummy/dummy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/rgb', pathMatch: 'full' },
+  { path: 'rgb', component: DummyComponent },
+  { path: 'buttons', component: DummyComponent },
+  { path: 'dpi', component: DummyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

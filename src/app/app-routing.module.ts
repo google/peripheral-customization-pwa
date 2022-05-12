77e1d0de94d44d9fdf4f83c59af95d0004479d3e
MatheusCod <matheus.fernandes@profusion.mobi>
2022-05-12 15:05:14 -0300

    chore: implement Adjust DPI component in Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DummyComponent } from './dummy/dummy.component';
 import { CustomizeButtonsComponent } from './pane/customize-buttons/customize-buttons.component';
+import { AdjustDpiComponent } from './pane/adjust-dpi/adjust-dpi.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/rgb', pathMatch: 'full' },
   { path: 'rgb', component: DummyComponent },
   { path: 'buttons', component: CustomizeButtonsComponent },
-  { path: 'dpi', component: DummyComponent },
+  { path: 'dpi', component: AdjustDpiComponent },
 ];
 
 @NgModule({

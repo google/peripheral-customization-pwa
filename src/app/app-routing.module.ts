8bd981bf95b1808cd0dbf4563b747934769b9249
MatheusCod <matheus.fernandes@profusion.mobi>
2022-05-12 14:53:27 -0300

    chore: implement CustomizeButtons component in Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DummyComponent } from './dummy/dummy.component';
+import { CustomizeButtonsComponent } from './pane/customize-buttons/customize-buttons.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/rgb', pathMatch: 'full' },
   { path: 'rgb', component: DummyComponent },
-  { path: 'buttons', component: DummyComponent },
+  { path: 'buttons', component: CustomizeButtonsComponent },
   { path: 'dpi', component: DummyComponent },
 ];
 
